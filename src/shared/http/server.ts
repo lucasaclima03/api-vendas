ee4e674c9cac1f9fa0d63f539f6b62d436e20811
Lucas Almeida <lucasaclima03@gmail.com>
2022-02-12 13:11:11 -0300

    conectando ao banco de dados

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,7 +1,9 @@
+import 'reflect-metadata';
 import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
 import { AppError } from '../errors/AppError';
+import '@shared/typeorm';
 
 const app = express();
 
@@ -10,25 +12,20 @@ app.use(express.json());
 app.use(routes);
 
 app.use(
-    (
-        error: Error,
-        request: Request,
-        response: Response,
-        next: NextFunction,
-    ) => {
-        if (error instanceof AppError) {
-            return response.status(error.statusCode).json({
-                status: 'error',
-                message: error.message,
-            });
-        }
-        return response.status(500).json({
-            status: 'error',
-            message: 'Internal server error',
-        });
-    },
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal server error',
+    });
+  },
 );
 
 app.listen(8080, () => {
-    console.log('Server started on port 3333!');
+  console.log('Server started on port 3333!');
 });
