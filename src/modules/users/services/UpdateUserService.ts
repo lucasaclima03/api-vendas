726f3f43f6da16e721e0415c1f9cd9b7219a5a55
Lucas Almeida <lucasaclima03@gmail.com>
2022-02-24 12:43:28 -0300

    criação do servico para update de usuario

---
+++ src/modules/users/services/UpdateUserService.ts
@@ -0,0 +1,34 @@
+import { AppError } from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import User from '../typeorm/entities/User';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+
+interface IRequest {
+  id: string;
+  name: string;
+  email: string;
+  password: string;
+}
+
+class UpdateUserService {
+  public async execute({ id, name, email, password }: IRequest): Promise<User> {
+    const usersRepository = getCustomRepository(UsersRepository);
+    const user = await usersRepository.findById(id);
+    if (!user) {
+      throw new AppError('User not found');
+    }
+
+    const emailExists = await usersRepository.findByEmail(email);
+    if (emailExists) {
+      throw new AppError('This name is already used');
+    }
+
+    user.name = name;
+    user.email = email;
+    user.password = password;
+
+    return user;
+  }
+}
+
+export default UpdateUserService;
