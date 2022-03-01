dacddecceb61e021017a348c7d2817fb66ce88bb
Lucas Almeida <lucasaclima03@gmail.com>
2022-03-01 16:12:28 -0300

    sessão de autenticação com token

--- src/modules/users/services/CreateSessionsService.ts
+++ src/modules/users/services/CreateSessionsService.ts
@@ -1,5 +1,6 @@
 import { AppError } from '@shared/errors/AppError';
 import { compare, hash } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 import { getCustomRepository } from 'typeorm';
 import User from '../typeorm/entities/User';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
@@ -8,9 +9,13 @@ interface IRequest {
   email: string;
   password: string;
 }
+interface IResponse {
+  user: User;
+  token: string;
+}
 
 class CreateSessionsService {
-  public async execute({ email, password }: IRequest): Promise<User> {
+  public async execute({ email, password }: IRequest): Promise<IResponse> {
     const usersRepository = await getCustomRepository(UsersRepository);
     const user = await usersRepository.findByEmail(email);
     if (!user) {
@@ -23,7 +28,15 @@ class CreateSessionsService {
       throw new AppError('Incorrect user/password', 401);
     }
 
-    return user;
+    const token = sign({}, '98855a7f3b2b98b776cf8c8e2076ace5', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
+    return {
+      user,
+      token,
+    };
   }
 }
 
