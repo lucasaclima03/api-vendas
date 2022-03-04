cd2cab9a2e3371833475505b22514b4d8568ea56
Lucas Almeida <lucasaclima03@gmail.com>
2022-03-04 10:22:33 -0300

    criação da entidade usertoken

---
+++ src/modules/users/typeorm/entities/UserToken.ts
@@ -0,0 +1,26 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  Generated,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+class UserToken {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  @Generated('uuid')
+  token: string;
+
+  @Column()
+  user_id: string;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default UserToken;
